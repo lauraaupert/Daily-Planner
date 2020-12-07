a84aceec12d306f75baa794fda2b4021441ad4ba
Laura Aupert <aupert.laura@gmail.com>
2020-12-06 19:49:13 -0500

    with rows cloned and a mess of a code

--- script.js
+++ script.js
@@ -21,41 +21,148 @@ events.forEach( event => {
 })
 
 
-
+// the rows will be in an array that will include an array of task for each row
 
 
 
 const currentDay = document.querySelector("#currentDay");
 currentDay.textContent = dayjs().format("dddd, MMMM D, YYYY");
 
-var allRows = document.querySelector(".extrablocks");
+const currentTime = document.querySelector("#currentTime");
+currentTime.textContent = dayjs().format("hh:mm:ss")
+
+//var allRows = document.querySelector(".extrablocks");
 var createRows = [];
 
-for (var i = 0; i < 9; i++) {
-    $(".row").clone().push(createRows);
-    createRows.appendto(allRows);
-}
 
 //when click saveBtn create new text area in same time-block
 
-//to clone the row if I don't want to write all time blocks
-/*function addRow(id)
-    { var newRow=document.getElementById("#row").tBodies[0];  //get the table
-      var node=t.rows[0].cloneNode(true);    //clone the previous node or row
-      newRow.appendChild(node);   //add the node or row to the table
-    }  
-    addRow()*/
     var storedTodos = JSON.parse(localStorage.getItem("todos"));
     var todoInput = document.querySelector("#todo-text");
 
-    $(".saveBtn").on("click", function(event) {
-        $(".todo").css("float", "left");
-        var area = $("<textarea>");
-        area.css("float", "left")
-        $(".saveBtn").before(area)
+    var saveBtn = $(".saveBtn")
+    var row = $(".row")
+    //var allRows = [
+     //   {row: ["yellow", "blue"]
+    //    },
+    //    {row: []}, 
+//]
+    var rowA = $("#0")
+    //.attr("id", 0)
+    //i=0
+    //0.attr("id", "row"+i)
 
-    }
-    )
+
+    //var allRows = [$(".row")]
+
+//allRows.push({row: ["hello"]})
+//console.log(allRows[2].row[0])
+console.log(allRows)
+//allRows.push(row)
+//saveBtn.append(row)
+
+    //var row_id = $("#0:nth-child(1)").last().attr("id");
+    //var split_id = row_id.split();
+  
+    // New index
+    //var index = Number([1]) + 1;
+//var index = Number([1]) + 1;
+//$(newRows).find('0:nth-child(1)').attr("id","0"+index);
+  //$(newRows).find('0:nth-child(2)').attr("id", "0"+index);
+  // Set value
+  //$(newRows).find('0:nth-child(1)').val("row"+index);
+  //$(newRows).find('0:nth-child(2)').val("row"+index);
+
+ // var newRows = allRows.push($("#0").clone().appendTo(".extrablocks"))
+for (i = 0; i < 9; i++) {
+var time = 9;
+    var newRows = $("#0").clone().attr("id", i+1).appendTo(".extrablocks")
+    
+    $(".time").text(time++)
+}
+    var allRows = $(".row")
+    //$("#0").
+
+
+console.log($(".row"))
+
+
+
+    //allRows.push(newRows)
+  //console.log($(".row"))
+
+  //var allNewRows = allRows.add($("#timeblocks").children())
+
+   //$("#timeblocks").children().each(function () {
+    //newRows.attr("id", i)
+    //var allRowsNew = $(allRows).add(newRows)
+   //})
+    //console.log(allRowsNew)
+
+
+
+  //allRows.push(newRows)
+  //allRows[0].push({task: "hello"})
+  //var allRows = [$(".row")]
+    console.log(allRows)
+    console.log(allRows[0])
+    console.log(allRows[1])
+    console.log(allRows[2])
+    console.log(allRows[9])
+
+
+
+  //$each(rowA)
+
+  //$("#timeblocks").children("#rowA").each(i++)
+   // console.log(rowA)
+
+var time = 9;
+$(".time").text(time)
+
+//console.log(newRows[3])
+
+
+//console.log(allRows[0].row[0])
+
+
+//or row = allRows[i]
+    //console.log(allRows[0])
+
+    var currentRow = 0
+
+
+    
+
+    $(".saveBtn").on("click", function(event) {
+        event.preventDefault();
+        currentRow = parseInt(event.target.parentElement.id);
+        console.log(currentRow)
+        var area = $("<textarea>");
+        var task = area.value;
+        console.log(area.value)
+        console.log(event)
+//should have an if it's blank do nothing
+        if (area) {
+            task = area.value;
+        }
+        //$(".todo").css("float", "left");
+
+        //area.css("float", "left")
+        $("#col-md-1").append("<textarea>")
+        console.log(area.value)
+
+        allRows[currentRow].push({task: task})
+        console.log(allRows)
+
+
+
+  //$(area).text(allRows[0].newRow)
+
+//when click saveBtn 
+
+
+  });
     
     /*addStuff())
     
